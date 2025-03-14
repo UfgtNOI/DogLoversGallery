0deb4a42aec6032e385ea78e5a955a13193937c6
Ufgt <Strelok1579@gmail.com>
2025-03-15 04:46:45 +0700

    Обновляем вьюмодел

--- app/src/main/java/com/example/dogloversgallery/viewmodel/DogViewModel.kt
+++ app/src/main/java/com/example/dogloversgallery/viewmodel/DogViewModel.kt
@@ -4,22 +4,37 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.dogloversgallery.network.RetrofitClient
+import com.example.dogloversgallery.database.FavoriteDog
+import com.example.dogloversgallery.repository.DogRepository
 import kotlinx.coroutines.launch
 
-class DogViewModel : ViewModel() {
+class DogViewModel(private val repository: DogRepository) : ViewModel() {
 
     private val _dogImage = MutableLiveData<String>()
     val dogImage: LiveData<String> get() = _dogImage
 
+    val favorites: LiveData<List<FavoriteDog>> = repository.getFavorites()
+
     fun loadRandomDogImage() {
         viewModelScope.launch {
             try {
-                val response = RetrofitClient.instance.getRandomDogImage()
+                val response = repository.getRandomDogImage()
                 _dogImage.value = response.imageUrl
             } catch (e: Exception) {
                 // Обработка ошибок
             }
+        }
+    }
+
+    fun addToFavorites(imageUrl: String) {
+        viewModelScope.launch {
+            repository.addToFavorites(imageUrl)
+        }
+    }
+
+    fun removeFromFavorites(imageUrl: String) {
+        viewModelScope.launch {
+            repository.removeFromFavorites(imageUrl)
         }
     }
 }
